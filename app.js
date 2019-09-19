113f2fb2f562e2c681bbdd7b6b53b74fd13a3f19
sakibashrak <officialsakibashrak@gmail.com>
2019-09-19 17:52:53 +0600

    first commit - weather app using VJS

---
+++ app.js
@@ -0,0 +1,63 @@
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degree');
+  let locationTimezone = document.querySelector('.location-timezone');
+  let temperatureSection = document.querySelector('.temperature');
+  let temperatureSpan = document.querySelector('.temperature span');
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude.toFixed(4);
+      lat = position.coords.latitude.toFixed(4);
+
+
+      const proxy = "https://cors-anywhere.herokuapp.com/";
+      const api = `${proxy}https://api.darksky.net/forecast/143b70648970fbafa7d8bbe4ab465746/${lat},${long}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const { temperature, summary, icon } = data.currently;
+          //  Set dom elements from api
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+          locationTimezone.textContent = data.timezone;
+          //Set icon
+          setIcons(icon, document.querySelector(".icon"));
+          //formula
+          let celsius = (temperature - 32) * (5 / 9);
+          // change temperature to celsius/Farenheit
+          temperatureSection.addEventListener('click', () => {
+            if (temperatureSpan.textContent === "F") {
+              temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
+            } else {
+              temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
+            }
+          })
+        });
+    });
+
+  }
+
+
+
+  else {
+    h1.textContent = "please enble your geolocation ..."
+  }
+
+
+
+
+  function setIcons (icon, iconID) {
+    const skycons = new Skycons({ "color": "white" });
+    const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+    skycons.play();
+    return skycons.set(iconID, Skycons[currentIcon]);
+  }
+});
